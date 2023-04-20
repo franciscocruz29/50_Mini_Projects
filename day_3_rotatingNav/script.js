c1b31dd6d1a6cc1d31a1ad775319c63de9c41709
franciscocruz29 <franciscohuertas25@gmail.com>
2023-04-20 14:57:25 -0500

    Added nav and content styles

--- day_3_rotatingNav/script.js
+++ day_3_rotatingNav/script.js
@@ -2,6 +2,7 @@ const open = document.getElementById('open');
 const close = document.getElementById('close');
 const container = document.querySelector('.container');
 
+// use the classList API to add and remove the show-nav class
 open.addEventListener('click', () => container.classList.add('show-nav'));
 
 close.addEventListener('click', () => container.classList.remove('show-nav'));
