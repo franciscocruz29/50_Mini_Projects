f08648d6e9f12e5e673fe810b6b2406aab290060
franciscocruz29 <franciscohuertas25@gmail.com>
2023-05-05 10:00:00 -0500

    Refactored javascript day 11

--- day_11_eventKeycodes/script.js
+++ day_11_eventKeycodes/script.js
@@ -1,26 +1,30 @@
-const insert = document.getElementById('insert');
-
 /* Each of these div elements contains the following child elements:
 
 The first child element is a template literal ${ event.key === ' ' ? 'Space' : event.key; }, which displays the key that was pressed. If the key pressed is the space bar, then the text "Space" is displayed instead of a space character.
 The second child element is the keyCode property of the event object, which contains the numerical value of the key that was pressed.
 The third child element is the code property of the event object, which contains a string representation of the key that was pressed. */
 
-window.addEventListener('keydown', (event) => {
-  insert.innerHTML = `
-  <div class="key">
-  ${event.key === ' ' ? 'Space' : event.key} 
-  <small>event.key</small>
-</div>
+const insert = document.getElementById('insert');
 
-<div class="key">
-  ${event.keyCode}
-  <small>event.keyCode</small>
-</div>
+function displayKeyInfo(event) {
+  const keyName = event.key === ' ' ? 'Space' : event.key;
+  const keyCode = event.keyCode;
+  const code = event.code;
 
-<div class="key">
-  ${event.code}
-  <small>event.code</small>
-</div>
+  const html = `
+    <div class="key">
+      ${keyName} <small>event.key</small>
+    </div>
+    <div class="key">
+      ${keyCode} <small>event.keyCode</small>
+    </div>
+    <div class="key">
+      ${code} <small>event.code</small>
+    </div>
   `;
-});
+
+  insert.innerHTML = html;
+}
+
+window.addEventListener('keydown', displayKeyInfo);
+
