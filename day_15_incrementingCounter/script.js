faedfe34a7474128024ba04c4cd7cdd5ae734306
franciscocruz29 <franciscohuertas25@gmail.com>
2023-05-29 21:35:12 -0500

    Refactored javascript day_15

--- day_15_incrementingCounter/script.js
+++ day_15_incrementingCounter/script.js
@@ -1,38 +1,27 @@
-// Query all elements with the class 'counter' and assign them to the variable 'counters'
+// Function to handle the update of counter
+const updateCounter = (counter, target) => {
+  let currentCount = +counter.innerText;
+  const increment = target / 200;
+
+  if (currentCount < target) {
+    counter.innerText = `${Math.ceil(currentCount + increment)}`;
+    setTimeout(() => updateCounter(counter, target), 1);
+  } else {
+    counter.innerText = target;
+  }
+};
+
+// Query all elements with the class 'counter'
 const counters = document.querySelectorAll('.counter');
 
 // Loop over each 'counter' element
 counters.forEach(counter => {
-
   // Initialize the innerText of the counter to '0'
   counter.innerText = '0';
 
-  // Define a function called 'updateCounter'
-  const updateCounter = () => {
+  // Get the 'data-target' attribute from the counter element and convert it into a number
+  const target = +counter.getAttribute('data-target');
 
-    // Get the 'data-target' attribute from the counter element and convert it into a number
-    const target = +counter.getAttribute('data-target');
-
-    // Get the current innerText of the counter and convert it into a number
-    const c = +counter.innerText;
-
-    // Calculate the increment value. This is the target value divided by 200
-    const increment = target / 200;
-
-    // If the current value is less than the target value
-    if (c < target) {
-      // Increase the counter by the increment value and round it up to the nearest integer, then set this as the new counter text
-      counter.innerText = `${Math.ceil(c + increment)}`;
-
-      // Call the 'updateCounter' function after 1 millisecond
-      setTimeout(updateCounter, 1);
-    } else {
-      // If the current value has reached (or exceeded) the target value, set the counter text to the target value
-      counter.innerText = target;
-    }
-  };
-
-  // Call the 'updateCounter' function to start the counter
-  updateCounter();
-
+  // Start the counter
+  updateCounter(counter, target);
 });
