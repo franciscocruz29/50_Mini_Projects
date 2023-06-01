0cd37f7ef31101cf6ad128805ba17af06b457342
franciscocruz29 <franciscohuertas25@gmail.com>
2023-06-01 14:42:25 -0500

    Refactored javascript day_18

--- day_18_backgroundSlider/script.js
+++ day_18_backgroundSlider/script.js
@@ -1,63 +1,66 @@
-// Retrieve a reference to the <body> of the HTML document
-const body = document.body;
+// The logic is largely the same, but instead of a series of global variables and functions, 
+// there's now a SlideShow class that encapsulates all the functionality.
 
-// Select all elements that have the class 'slide'
-const slides = document.querySelectorAll('.slide');
+// Create a SlideShow class to handle slide functionality
+class SlideShow {
+  constructor(body, slides, leftBtn, rightBtn) {
+    this.body = body;
+    this.slides = slides;
+    this.leftBtn = leftBtn;
+    this.rightBtn = rightBtn;
+    this.activeSlideIndex = 0;
 
-// Get the button elements with the id 'left' and 'right'
-const leftBtn = document.getElementById('left');
-const rightBtn = document.getElementById('right');
+    // Set initial body background
+    this.setBodyBackground();
 
-// Initialize a counter to keep track of the active slide
-let activeSlide = 0;
+    // Set initial active slide
+    this.setActiveSlide();
 
-// Add a 'click' event listener to the right button
-rightBtn.addEventListener('click', () => {
-  // Increment the active slide counter
-  activeSlide++;
-
-  // If activeSlide exceeds the total number of slides, reset it to 0
-  if (activeSlide > slides.length - 1) {
-    activeSlide = 0;
+    // Bind event handlers
+    this.leftBtn.addEventListener('click', this.previousSlide.bind(this));
+    this.rightBtn.addEventListener('click', this.nextSlide.bind(this));
   }
 
-  // Change the body's background to the current slide's background
-  setBgToBody();
-
-  // Set the current slide as active
-  setActiveSlide();
-});
-
-// Add a 'click' event listener to the left button
-leftBtn.addEventListener('click', () => {
-  // Decrement the active slide counter
-  activeSlide--;
-
-  // If activeSlide goes below 0, set it to the last slide
-  if (activeSlide < 0) {
-    activeSlide = slides.length - 1;
+  // Set the body's background image to the current slide's background
+  setBodyBackground() {
+    this.body.style.backgroundImage = this.slides[this.activeSlideIndex].style.backgroundImage;
   }
 
-  // Change the body's background to the current slide's background
-  setBgToBody();
+  // Set the active slide
+  setActiveSlide() {
+    this.slides.forEach(slide => slide.classList.remove('active'));
+    this.slides[this.activeSlideIndex].classList.add('active');
+  }
 
-  // Set the current slide as active
-  setActiveSlide();
-});
+  // Go to the next slide
+  nextSlide() {
+    this.activeSlideIndex++;
+    if (this.activeSlideIndex > this.slides.length - 1) {
+      this.activeSlideIndex = 0;
+    }
+    this.updateSlide();
+  }
 
-// Initially, set the body's background to the first slide's background
-setBgToBody();
+  // Go to the previous slide
+  previousSlide() {
+    this.activeSlideIndex--;
+    if (this.activeSlideIndex < 0) {
+      this.activeSlideIndex = this.slides.length - 1;
+    }
+    this.updateSlide();
+  }
 
-// Function to set the body's background image to the current slide's background
-function setBgToBody() {
-  body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
+  // Update the slide
+  updateSlide() {
+    this.setBodyBackground();
+    this.setActiveSlide();
+  }
 }
 
-// Function to set the active slide
-function setActiveSlide() {
-  // For each slide, remove the 'active' class
-  slides.forEach((slide) => slide.classList.remove('active'));
-
-  // Add the 'active' class to the current slide
-  slides[activeSlide].classList.add('active');
-}
+// Instantiate the SlideShow class
+new SlideShow(
+  document.body,
+  document.querySelectorAll('.slide'),
+  document.getElementById('left'),
+  document.getElementById('right')
+);
