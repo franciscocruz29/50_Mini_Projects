4ca516697e3b7e4a6712826ab6050c8bf098daf1
franciscocruz29 <franciscohuertas25@gmail.com>
2023-06-05 15:51:24 -0500

    Refactored javascript day_19

--- day_19_themeClock/script.js
+++ day_19_themeClock/script.js
@@ -1,55 +1,70 @@
-// First, we grab HTML elements using the querySelector method.
-const hourEl = document.querySelector('.hour');
-const minuteEl = document.querySelector('.minute');
-const secondEl = document.querySelector('.second');
-const timeEl = document.querySelector('.time');
-const dateEl = document.querySelector('.date');
-const toggle = document.querySelector('.toggle');
-
-// Arrays containing the days of the week and the months in a year
-const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-
-// Here we add an event listener to the toggle element, so when it's clicked, the theme changes from light to dark or vice versa
-toggle.addEventListener('click', (e) => {
-  const html = document.querySelector('html');
-  if (html.classList.contains('dark')) {
-    html.classList.remove('dark'); // If dark mode is on, remove it
-    e.target.innerHTML = 'Dark mode'; // and change the button text to "Dark mode"
-  } else {
-    html.classList.add('dark'); // If dark mode is off, turn it on
-    e.target.innerHTML = 'Light mode'; // and change the button text to "Light mode"
-  }
-});
-
-// The setTime function is responsible for updating the time and date displayed
-function setTime() {
-  const time = new Date(); // Gets the current date and time
-  const month = time.getMonth(); // Gets the current month
-  const day = time.getDay(); // Gets the current day of the week
-  const date = time.getDate(); // Gets the current date of the month
-  const hours = time.getHours(); // Gets the current hour
-  const hoursForClock = hours >= 13 ? hours % 12 : hours; // Converts 24-hour time to 12-hour time for display
-  const minutes = time.getMinutes(); // Gets the current minutes
-  const seconds = time.getSeconds(); // Gets the current seconds
-  const ampm = hours >= 12 ? 'PM' : 'AM'; // Determines whether it's AM or PM
-
-  // Updates the position of the hour, minute, and second hands on the clock
-  hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 12, 0, 360)}deg)`;
-  minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 60, 0, 360)}deg)`;
-  secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 60, 0, 360)}deg)`;
-
-  // Updates the time and date display
-  timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`;
-  dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`;
-}
-
-// The scale function is used to map the time values to degrees for rotating the clock hands
-const scale = (num, in_min, in_max, out_min, out_max) => {
-  return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+// DOM elements
+const domElements = {
+  hour: document.querySelector('.hour'),
+  minute: document.querySelector('.minute'),
+  second: document.querySelector('.second'),
+  time: document.querySelector('.time'),
+  date: document.querySelector('.date'),
+  toggle: document.querySelector('.toggle')
 };
 
-setTime(); // Call the setTime
+// Constants for days and months
+const timeConstants = {
+  days: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+  months: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+};
 
-// Set the time every second
-setInterval(setTime, 1000);
+// Toggle event for changing theme
+domElements.toggle.addEventListener('click', (e) => {
+  const html = document.querySelector('html');
+  html.classList.toggle('dark');
+  e.target.innerHTML = html.classList.contains('dark') ? 'Light mode' : 'Dark mode';
+});
+
+// Function to update time and date
+function updateTimeAndDate() {
+  const currentTime = new Date();
+  const hour = currentTime.getHours();
+  const minute = currentTime.getMinutes();
+  const second = currentTime.getSeconds();
+
+  // Convert 24-hour format to 12-hour format
+  const hourForClock = hour >= 13 ? hour % 12 : hour;
+  const ampm = hour >= 12 ? 'PM' : 'AM';
+
+  // Rotate the clock hands
+  rotateClockHands(hourForClock, minute, second);
+
+  // Update the time and date display
+  displayTimeAndDate(hourForClock, minute, ampm, currentTime);
+}
+
+function rotateClockHands(hour, minute, second) {
+  domElements.hour.style.transform = createRotationStyle(hour, 0, 12, 0, 360);
+  domElements.minute.style.transform = createRotationStyle(minute, 0, 60, 0, 360);
+  domElements.second.style.transform = createRotationStyle(second, 0, 60, 0, 360);
+}
+
+function createRotationStyle(num, in_min, in_max, out_min, out_max) {
+  const degree = scale(num, in_min, in_max, out_min, out_max);
+  return `translate(-50%, -100%) rotate(${degree}deg)`;
+}
+
+function displayTimeAndDate(hour, minute, ampm, currentTime) {
+  const day = currentTime.getDay();
+  const month = currentTime.getMonth();
+  const date = currentTime.getDate();
+
+  domElements.time.innerHTML = `${hour}:${minute < 10 ? `0${minute}` : minute} ${ampm}`;
+  domElements.date.innerHTML = `${timeConstants.days[day]}, ${timeConstants.months[month]} <span class="circle">${date}</span>`;
+}
+
+// Function to scale time to degree for clock hands rotation
+function scale(num, in_min, in_max, out_min, out_max) {
+  return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+updateTimeAndDate(); // Call the updateTimeAndDate function
+
+// Set interval to update the time every second
+setInterval(updateTimeAndDate, 1000);
