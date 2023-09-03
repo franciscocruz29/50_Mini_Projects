176e20c7778f2f1377463c75851891b83d276bb1
Francisco Cruz <franciscohuertas25@gmail.com>
2023-09-03 05:54:56 -0500

    Refactored Javascript

--- day_34_animatedCountdown/script.js
+++ day_34_animatedCountdown/script.js
@@ -1,81 +1,54 @@
 /* This code animates a series of numbers sequentially by adding/removing CSS classes and using animationend event listeners. 
 When complete, it hides a counter, shows a final message, and allows restarting the animation. */
 
-// Select all elements with class 'nums' and span tags
-const nums = document.querySelectorAll('.nums span');
+/* In this refactored version, the code is encapsulated within an IIFE to prevent global scope pollution, 
+constants are used for selectors, and the functions are defined separately for better organization and readability. */
 
-// Select element with class 'counter'
-const counter = document.querySelector('.counter');
+(function () {
+  // Define constants for class names and elements to improve code readability.
+  const NUMS_SELECTOR = '.nums span';
+  const COUNTER_SELECTOR = '.counter';
+  const FINAL_SELECTOR = '.final';
+  const REPLAY_SELECTOR = '#replay';
 
-// Select element with class 'final' 
-const finalMessage = document.querySelector('.final');
+  const nums = document.querySelectorAll(NUMS_SELECTOR);
+  const counter = document.querySelector(COUNTER_SELECTOR);
+  const finalMessage = document.querySelector(FINAL_SELECTOR);
+  const replay = document.querySelector(REPLAY_SELECTOR);
 
-// Select element with id 'replay'
-const replay = document.querySelector('#replay');
-
-// Call runAnimation function to start animation
-runAnimation();
-
-// Function to reset/clear animation classes
-function resetDOM() {
-
-  // Hide counter element  
-  counter.classList.remove('hide');
-
-  // Hide final message
-  finalMessage.classList.remove('show');
-
-  // Remove animation classes from all nums elements
-  nums.forEach((num) => {
-    num.classList.value = '';
-  });
-
-  // Add 'in' class to first num element
-  nums[0].classList.add('in');
-}
-
-// Main animation function
-function runAnimation() {
-
-  // Loop through each num element
-  nums.forEach((num, idx) => {
-
-    // Get index of next to last element
-    const nextToLast = nums.length - 1;
-
-    // Add event listener for animation end
-    num.addEventListener('animationend', (e) => {
-
-      // If animation that ended is 'goIn' and not the last element
-      if (e.animationName === 'goIn' && idx !== nextToLast) {
-
-        // Remove 'in' class and add 'out' class
-        num.classList.remove('in');
-        num.classList.add('out');
-
-        // If animation that ended is 'goOut' and element has next sibling
-      } else if (e.animationName === 'goOut' && num.nextElementSibling) {
-
-        // Add 'in' class to next sibling 
-        num.nextElementSibling.classList.add('in');
-
-        // Otherwise, this is the last animation
-      } else {
-
-        // Hide counter
-        counter.classList.add('hide');
-
-        // Show final message 
-        finalMessage.classList.add('show');
-      }
-    });
-  });
-}
-
-// Add click handler to replay button 
-replay.addEventListener('click', () => {
-
-  // Reset DOM and restart animation
-  resetDOM();
+  // Initialize the animation when the page loads.
   runAnimation();
-});
+
+  // Reset the DOM to its initial state.
+  function resetDOM() {
+    counter.classList.remove('hide');
+    finalMessage.classList.remove('show');
+    nums.forEach((num) => num.classList.value = '');
+    nums[0].classList.add('in');
+  }
+
+  // Run the animation.
+  function runAnimation() {
+    nums.forEach((num, idx) => {
+      const nextToLast = nums.length - 1;
+
+      num.addEventListener('animationend', (e) => {
+        if (e.animationName === 'goIn' && idx !== nextToLast) {
+          num.classList.remove('in');
+          num.classList.add('out');
+        } else if (e.animationName === 'goOut' && num.nextElementSibling) {
+          num.nextElementSibling.classList.add('in');
+        } else {
+          counter.classList.add('hide');
+          finalMessage.classList.add('show');
+        }
+      });
+    });
+  }
+
+  // Add a click event listener to the 'replay' button.
+  replay.addEventListener('click', () => {
+    resetDOM();
+    runAnimation();
+  });
+})();
